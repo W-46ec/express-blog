68d4200f877895efeb9e096baa4b44c6d3e32411
W-46ec <30034417+W-46ec@users.noreply.github.com>
2017-08-25 14:40:20 +0800

    优化消息模块

--- tools/msg.js
+++ tools/msg.js
@@ -1,3 +1,13 @@
+//信息模板
+var message = function(res, title, text, path){
+	res.status(200);
+	res.render('msg', {
+		title: title,
+		text: text,
+		path: path
+	});
+}
+
 //数据库操作错误信息
 var dbError = function(res){
 	res.status(500);
@@ -8,6 +18,7 @@ var dbError = function(res){
 	});
 }
 
+//404错误信息
 var error404 = function(res){
 	res.status(404);
 	res.render('err404', {
@@ -15,7 +26,41 @@ var error404 = function(res){
 	});
 }
 
+//登陆错误信息
+var loginMsg = function(res){
+	res.status(200);
+	res.render('msg', {
+		title: 'Login', 
+		text: 'Incorrect username or password.', 
+		path: '/users/login.html'
+	});
+}
+
+//成功信息
+var succMsg = function(res){
+	res.status(200);
+	res.render('msg', {
+		title: 'Success', 
+		text: 'Success!', 
+		path: '/lists'
+	});
+}
+
+//拒绝访问
+var accessDeniedMsg = function(res){
+	res.status(200);
+	res.render('msg', {
+		title: 'Access denied', 
+		text: 'Access denied!', 
+		path: '/lists'
+	});
+}
+
 module.exports = {
+	message: message,
 	dbError: dbError,
-	error404: error404
+	error404: error404,
+	loginMsg: loginMsg,
+	succMsg: succMsg,
+	accessDeniedMsg: accessDeniedMsg
 }
